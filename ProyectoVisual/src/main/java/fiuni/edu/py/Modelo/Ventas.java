aea3c29f8080690a286c13008b83e9e130e0ac65
santi <santi@DESKTOP-LFK5GQI>
2025-05-30 20:54:32 -0400

    Agrego  de clases

--- ProyectoVisual/src/main/java/fiuni/edu/py/Modelo/Ventas.java
+++ ProyectoVisual/src/main/java/fiuni/edu/py/Modelo/Ventas.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -132,5 +133,4 @@ public class Ventas {
     public void setTotal(int total) {
         this.total = total;
     }
-
 }
